8541e477ae6c0afcae395ff3beffc3f0a4326453
Basasuya <961256668@qq.com>
2018-02-07 19:46:12 +0800

    a new modify

--- A.cpp
+++ A.cpp
@@ -1,42 +1,16 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-void replace(string a, string b, string c) {
-	regex reg(b);
-	string result = regex_replace(a, reg, c);
-	cout << result << endl;
-}
-void count(string a, string b) {
-	regex reg(b);
-	smatch sm;
-	int cnt = 0;
-	while( regex_search(a, sm, reg) ) {
-		cnt ++;
-		a = sm.suffix().str();
-	}
-	cout << cnt << endl;
-}
-void search(string a, string b) {
-	regex reg(b);
-	smatch sm;
-	int t1 = a.length();
-	int t2, t3;
-	bool flag = regex_search(a, sm, reg);
-	t3 = sm.begin()->str().length();
-	a = sm.suffix().str();
-	t2 = a.length();
-	printf("%d %d %d\n", t1, t2, t3);
-}
-
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <vector>
+#include <set>
+#include <map>
+#include <deque>
+#include <stack>
+#include <queue>
+#include <algorithm>
 
 int main() {
-	string a, b, c;
-	while(1) {
-		getline(cin, a);
-		getline(cin, b);
-		//		replace(a, b, c);	
-		count(a, b);
-	//	search(a, b);
-	}
-	return 0;	
-}	
+
+}
