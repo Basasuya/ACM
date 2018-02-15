b481fcc9cc790aabffcd6432e619cffe038cc28f
Basasuya <961256668@qq.com>
2018-02-15 23:10:20 +0800

    mm

--- CodeForces/CF461 div2/A.cpp
+++ CodeForces/CF461 div2/A.cpp
@@ -1,8 +1,17 @@
 #include <cstdio>
 
+bool Judege(int copyToy, int originToy) {
+    int actionCopy = copyToy - originToy + 1;
+    if ((originToy == 1 && actionCopy == 0) || (originToy > 1 && actionCopy >= 0 && actionCopy % 2 == 0))
+        return true;
+    else
+        return false;
+}
+
 int main() {
     int x, y;
-    while (~scanf("%d %d")) {
-        }
+    while (~scanf("%d %d", &x, &y)) {
+        printf("%s\n", Judege(x, y) ? "Yes" : "No");
+    }
     return 0;
 }
