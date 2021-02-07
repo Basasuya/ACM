38a0b8c0b69e17849022e4f7512ecea24502d5bb
Basasuya <961256668@qq.com>
2021-02-07 23:54:37 +0800

    update

---
+++ CodeForces/CF700/C.cpp
@@ -0,0 +1,87 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <complex>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#define MP make_pair
+#define ll long long
+#define ld long double
+#define null NULL
+#define all(a) a.begin(), a.end()
+#define forn(i, n) for (int i = 0; i < n; ++i)
+#define sz(a) (int)a.size()
+#define lson l , m , rt << 1
+#define rson m + 1 , r , rt << 1 | 1
+#define bitCount(a)  __builtin_popcount(a)
+template<class T> int gmax(T &a, T b) { if (b > a) { a = b; return 1; } return 0; }
+template<class T> int gmin(T &a, T b) { if (b < a) { a = b; return 1; } return 0; }
+using namespace std;
+const int INF = 0x3f3f3f3f;
+string to_string(string s) { return '"' + s + '"'; }
+string to_string(const char* s) { return to_string((string) s); }
+string to_string(bool b) { return (b ? "true" : "false"); }
+template <typename A, typename B>
+string to_string(pair<A, B> p) { return "(" + to_string(p.first) + ", " + to_string(p.second) + ")"; }
+template <typename A>
+string to_string(A v) { bool first = true; string res = "{"; for (const auto &x : v) { if (!first) { res += ", "; } first = false; res += to_string(x); } res += "}"; return res; }
+void debug_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); }
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+int ask_and_read(int y) {
+    printf("? %d\n", y);
+    fflush(stdout);
+    int x;
+    scanf("%d", &x);
+    return x;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    if(n == 1) {
+        printf("! 1\n");
+        fflush(stdout);
+        return 0;
+    }
+
+    if(n == 2) {
+        if(ask_and_read(1) == 1) printf("! 1\n");
+        else printf("! 2\n");
+        fflush(stdout);
+        return 0;
+    }
+
+    int l_val = ask_and_read(1); int l = 1;
+    int r_val = ask_and_read(n); int r = n;
+
+    while(1) {
+        int mid = (l + r) / 2;
+        int mid_val = ask_and_read(mid);
+
+        if(mid_val - l_val > mid - l ) 
+    }
+
+
+}
